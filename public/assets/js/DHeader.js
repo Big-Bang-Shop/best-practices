e2122f39083898cda55e03378ad96552d3ca8deb
Daniel Versiane <daniel.versiane@gmail.com>
2021-10-08 12:14:21 -0300

    feat: remove button github in Header.

--- public/assets/js/DHeader.js
+++ public/assets/js/DHeader.js
@@ -8,12 +8,6 @@ class DHeader extends HTMLElement {
         <a onclick="gotoHome()">
           <h1>Best practices</h1>
         </a>
-
-        <section>
-          <a class="btn btn-github" href="https://github.com/Big-Bang-Shop/best-practices" target="_blank">
-            <i class="fab fa-github-alt"></i>
-          </a>
-        </section>
       </div>
     </header>
     `;
